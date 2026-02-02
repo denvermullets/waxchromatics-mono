794c202272541e95ad2480e161ea23aa877035c5
denvermullets <devmullets@gmail.com>
2026-02-02 17:09:21 -0500

    feat(artists): search results for artists

--- app/services/wax_api_client/search_artists.rb
+++ app/services/wax_api_client/search_artists.rb
@@ -2,21 +2,22 @@ module WaxApiClient
   class SearchArtists < Service
     BASE_URL = ENV.fetch('WAX_API_BASE_URL', 'http://localhost:3030')
 
-    def initialize(query:)
+    def initialize(query:, page: 1, per_page: 25)
       @query = query
+      @page = page
+      @per_page = per_page
     end
 
     def call
-      uri = URI("#{BASE_URL}/artists")
-      uri.query = URI.encode_www_form(query: @query)
+      response = HTTParty.get("#{BASE_URL}/artists", query: { query: @query, page: @page, limit: @per_page })
+      return { artists: [], total_pages: 0, total_count: 0 } unless response.success?
 
-      response = Net::HTTP.get_response(uri)
-      return [] unless response.is_a?(Net::HTTPSuccess)
-
-      parsed = JSON.parse(response.body)
-      parsed.fetch('data', [])
+      parsed = response.parsed_response
+      artists = parsed.fetch('data', [])
+      pagy = parsed.fetch('pagy', {})
+      { artists: artists, total_pages: pagy.fetch('last', 1), total_count: pagy.fetch('count', artists.size) }
     rescue StandardError
-      []
+      { artists: [], total_pages: 0, total_count: 0 }
     end
   end
 end
