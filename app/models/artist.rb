91b99c887a897c8eaa6aac98712310e3f461dc8b
denvermullets <devmullets@gmail.com>
2026-02-06 19:32:57 -0500

    fix(artists): update to not use external api

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ApplicationRecord
   has_many :release_artists, dependent: :destroy
   has_many :releases, through: :release_artists
+  has_many :release_groups, -> { distinct }, through: :releases
 
   validates :name, presence: true
 end
